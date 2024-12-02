a531802a4a7611ddb4416927894ef7c4a9990fc5
sjsanc <sjsanc@pm.me>
2024-12-02 12:53:32 +0700

    2015 - 20

--- 2015/20/index.ts
+++ 2015/20/index.ts
@@ -1,17 +1,37 @@
 import Solution from "../solution";
 
-type From = string
-type To = string
-type Replacement = { from: From, to: To }
-type Molecule = string
+// The presents each house receives is the sum of its factors
+// Apparently, this can be optimised using a prime factorisation method?
 
 class Day20 extends Solution {
     constructor() {
         super(__dirname);
     }
 
+    factorise(mult: number, cap?: number) {
+        let idx = 1;
+        while (true) {
+            const factors = []
+            for (let i = 1; i <= Math.sqrt(idx); i++) {
+                if (idx % i === 0) {
+                    factors.push(i)
+                    if (i !== idx / i) factors.push(idx / i)
+                }
+                if (cap && factors.length == cap) break
+            }
+            if (factors.reduce((a,b) => a + b, 0) * mult > Number(this.input)) {
+                return idx
+            } 
+            idx++
+        }
+    }
+
     partOne() {
-        return 0
+        return this.factorise(10)
+    }
+
+    partTwo() {
+        return this.factorise(11, 50)
     }
 }
 
