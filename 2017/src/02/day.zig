4eadae2aff4e0e6b003a42298ca42eae088a47d3
sjsanc <sjsanc@protonmail.com>
2026-01-06 17:22:42 +0000

    add zig day 2 part 2

--- 2017/src/02/day.zig
+++ 2017/src/02/day.zig
@@ -6,7 +6,7 @@ pub fn solve(allocator: std.mem.Allocator) !void {
     defer s.deinit();
 
     const part1 = try solvePart1(s.lines);
-    const part2 = try solvePart2();
+    const part2 = try solvePart2(s.lines, allocator);
 
     try s.write(part1, part2);
 }
@@ -27,6 +27,27 @@ fn solvePart1(lines: [][]const u8) !i32 {
     return sum;
 }
 
-fn solvePart2() !i32 {
-    return 0;
+fn solvePart2(lines: [][]const u8, allocator: std.mem.Allocator) !i32 {
+    var sum: i32 = 0;
+    var nums: std.ArrayList(i32) = .empty;
+    defer nums.deinit(allocator);
+
+    for (lines) |line| {
+        nums.clearRetainingCapacity(); // empty the array
+        var it = std.mem.tokenizeScalar(u8, line, '\t');
+        while (it.next()) |token| {
+            const num = try std.fmt.parseInt(i32, token, 10);
+            try nums.append(allocator, num);
+        }
+        std.mem.sort(i32, nums.items, {}, std.sort.asc(i32));
+        for (nums.items, 0..) |num1, i| {
+            for (nums.items[i + 1 ..]) |num2| {
+                if (@rem(num2, num1) == 0) {
+                    sum += @divExact(num2, num1);
+                    break;
+                }
+            }
+        }
+    }
+    return sum;
 }
